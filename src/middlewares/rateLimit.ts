c6c607aa2c29c0634d3e58be9f411ab4407136c9
Yorn90104 <bingxiao526@gmail.com>
2026-03-17 00:28:52 +0800

    feat: 調整全域速率限制，將每個 IP 每分鐘的最大請求數從 40 增加至 60

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -3,7 +3,7 @@ import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 
 export const globalRateLimit = rateLimit({
   windowMs: 60 * 1000, // 1 分鐘
-  max: 40,
+  max: 60, // 每個 IP 在 1 分鐘內最多只能發出 60 個請求
   message: { message: "請求過於頻繁" },
   keyGenerator: (req: Request) => ipKeyGenerator(req.ip ?? ''), // 使用來自每個請求的 req.ip（即客戶端的 IP 地址）來生成唯一的識別鍵 這樣系統就會對每個 IP 地址進行獨立的限流控制
 });
