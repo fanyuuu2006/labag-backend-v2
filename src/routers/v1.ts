75a007e0e71545841f79f9a9f89d0cebb2088714
Yorn90104 <bingxiao526@gmail.com>
2026-03-18 16:01:38 +0800

    feat: 新增 shares 路由至 v1 路由器

--- src/routers/v1.ts
+++ src/routers/v1.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { router as usersRouter } from "./v1/users";
 import { router as authRouter } from "./v1/auth";
 import { router as gameRouter } from "./v1/game";
+import { router as sharesRouter } from "./v1/shares";
 
 export const router = Router();
 
 router
   .use("/users", usersRouter)
   .use("/auth", authRouter)
-  .use("/game", gameRouter);
+  .use("/game", gameRouter)
+  .use("/shares", sharesRouter);
