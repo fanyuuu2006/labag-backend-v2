ef18c316cfa624757d76e9ea2a3c7883fcf28892
Yorn90104 <bingxiao526@gmail.com>
2026-03-19 15:28:32 +0800

    feat: 擴展 SPIN_BETS 列表以增加投注選項

--- src/libs/user_coins.ts
+++ src/libs/user_coins.ts
@@ -1,3 +1,5 @@
 export const INITIAL_COINS = 20000;
 export const SHARE_REWARD_COINS = 1000;
-export const SPIN_BETS = [100, 200, 500, 1000, 2000, 5000, 10000];
+export const SPIN_BETS = [
+  100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000,
+];
